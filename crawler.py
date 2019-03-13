7093fb88d1f199e2fbf731950a47793a7dc98659
marcoortu <=>
2019-03-13 14:27:15 +0100

    solution

---
+++ crawler.py
@@ -0,0 +1,33 @@
+import urllib2
+from urlparse import urlparse, urljoin
+
+from bs4 import BeautifulSoup
+
+
+class Page(object):
+
+    def __init__(self, address, links):
+        self.address, self.links = address, links
+
+
+def crawler(url, iteration=0, maxIterations=2, web=[]):
+    if iteration == maxIterations:
+        return web
+    try:
+        page = urllib2.urlopen(url.geturl()).read()
+    except Exception:
+        return
+    soup = BeautifulSoup(page, "html.parser")
+    links = []
+    for anchor in soup.findAll('a', href=True):
+        link = urlparse(anchor['href'])
+        if not link.netloc:
+            link = urlparse(urljoin(url.geturl(), link.path))
+        links += [link.geturl()]
+    visited = set([page.address for page in web])
+    links = list(set(links))
+    links = filter(lambda l: l not in visited, links)
+    web += [Page(url.geturl(), links)]
+    for link in links:
+        crawler(urlparse(link), iteration + 1, maxIterations, web)
+    return web
--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-if __name__ == "__main__":
-    print("hello world")
+import networkx as nx
+import pylab as plt
+from urlparse import urlparse
+
+from crawler import crawler
+
+if __name__ == '__main__':
+    url = urlparse('http://info.cern.ch/hypertext/WWW/TheProject.html')
+    web = crawler(url)
+    webGraph = nx.DiGraph()
+    edges = []
+    for page in web:
+        for link in page.links:
+            edges.append((hash(page.address), hash(link)))
+    webGraph.add_edges_from(edges)
+    nx.draw(webGraph)
+    plt.show()
+    pageRank = nx.pagerank(webGraph)
+    for page in web:
+        print page.address, pageRank[hash(page.address)]
